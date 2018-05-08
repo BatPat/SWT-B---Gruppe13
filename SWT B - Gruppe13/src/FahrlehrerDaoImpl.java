81178627ff380e3bf48f49cc5070eb140dc0040b
Unknown <Lennart.Werner1@gmx.de>
2018-05-08 12:26:27 +0200

    Added Serialization

--- SWT B - Gruppe13/src/FahrlehrerDaoImpl.java
+++ SWT B - Gruppe13/src/FahrlehrerDaoImpl.java
@@ -1,27 +1,58 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.List;
 
-public class FahrlehrerDaoImpl implements FahrlehrerDao {
-
-	//TODO serialisierung
+public class FahrlehrerDaoImpl implements FahrlehrerDao, Serializable {
 
 	@Override
 	public List<Fahrlehrer> getAlleFahrlehrer() {
 		return null;
 	}
 
+	private File generateFile(Fahrlehrer fahrlehrer) {
+		String home = System.getProperty("user.home");
+		File dir = new File(home + "/Downloads/Fahrschule/Fahrlehrer");
+		dir.mkdir();
+		return new File(dir.getAbsolutePath() + fahrlehrer.getName() + ".ser");
+	}
+
 	@Override
 	public void addFahrlehrer(Fahrlehrer fahrlehrer) {
-
+		try (FileOutputStream fos = new FileOutputStream(generateFile(fahrlehrer));
+				ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+			try {
+				oos.writeObject(fahrlehrer);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 	}
 
 	@Override
 	public void updateFahrlehrer(Fahrlehrer fahrlehrer) {
-
+		File td = generateFile(fahrlehrer);
+		td.delete();
+		try (FileOutputStream fos = new FileOutputStream(generateFile(fahrlehrer));
+				ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+			try {
+				oos.writeObject(fahrlehrer);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 	}
 
 	@Override
 	public void deleteFahrlehrer(Fahrlehrer fahrlehrer) {
-
+		File td = generateFile(fahrlehrer);
+		td.delete();
 	}
 
 }
--- SWT B - Gruppe13/src/Theoriestunde.java
+++ SWT B - Gruppe13/src/Theoriestunde.java
@@ -3,11 +3,22 @@ import java.util.List;
 public class Theoriestunde extends Stunde {
 	
 	private TheorieThema thema;
+	private long genid;
+	public long getGenid() {
+		return genid;
+	}
 
+	private static long counter = 0;
+
+	
 	@Override
 	public List<Person> getBeteiligtePersonen() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public Theoriestunde() {
+		this.genid = counter++;
 	}
 	
 }
