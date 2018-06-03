7a5784beb6147e04121bfc817bf475f8e1a0089a
Unknown <Lennart.Werner1@gmx.de>
2018-06-03 11:55:23 +0200

    Added HibernateUtil to create a session

---
+++ src/fachlogik/HibernateUtil.java
@@ -0,0 +1,29 @@
+package fachlogik;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernateUtil {
+	
+	private static SessionFactory sessionFactory;
+
+	public static SessionFactory createSessionFactory() {
+		Configuration configuration = new Configuration();
+		configuration.configure();
+		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+				.applySettings(configuration.getProperties()).build();
+		sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		return sessionFactory;
+	}
+	
+	public void saveObject(Object o) {
+	Session session = createSessionFactory().openSession();
+	session.beginTransaction();
+	session.save(o);
+	session.getTransaction().commit();
+	}
+}
--- src/fachlogik/Main.java
+++ src/fachlogik/Main.java
@@ -2,10 +2,12 @@ package fachlogik;
 
 import java.sql.*;
 
+
+
 public class Main {
 
 	public static void main(String[] args) {
-		 new Controller();
+//		 new Controller();
 //		String jdbcurl = "jdbc:mysql://localhost:3306/fahrschule?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
 //		String user = "fahrschule";
 //		String pw = "fahrschule";
@@ -15,5 +17,8 @@ public class Main {
 //		} catch (SQLException e) {
 //			e.printStackTrace();
 //		}
+		Fahrlehrer f = new Fahrlehrer("Stefan Terlau", "44723", "Dortmund", "Kaspergaeschen", "3");
+		HibernateUtil h = new HibernateUtil();
+		h.saveObject(f);
 	}
 }
