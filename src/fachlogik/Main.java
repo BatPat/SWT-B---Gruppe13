fae3cb820afbf866a207c64528c8d54fcc4b0e89
Unknown <Lennart.Werner1@gmx.de>
2018-06-05 12:38:46 +0200

    Refactoring -Deleting FahrschuelerDTO

--- src/fachlogik/Main.java
+++ src/fachlogik/Main.java
@@ -3,18 +3,9 @@ package fachlogik;
 public class Main {
 
 	public static void main(String[] args) {
-    	 new Controller();
-//		String jdbcurl = "jdbc:mysql://localhost:3306/fahrschule?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-//		String user = "fahrschule";
-//		String pw = "fahrschule";
-//		try {
-//			Connection con = DriverManager.getConnection(jdbcurl, user, pw);
-//			System.out.println("Connection Successfull");
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-//		Fahrlehrer f = new Fahrlehrer("Stefan Terlau", "44723", "Dortmund", "Kaspergaeschen", "3");
-//		HibernateUtil h = new HibernateUtil();
-//		h.saveObject(f);
+//    	 new Controller();
+	    Fahrlehrer f = new Fahrlehrer("Stefan Terlau", "44723", "Dortmund", "Kaspergaeschen", "3");
+		HibernateUtil h = new HibernateUtil();
+		h.saveObject(f);
 	}
 }
