0a298bc6ae47c4f6b4b54b6a65e1c410be3cbf0f
Unknown <b.patti@gmx.de>
2018-06-05 08:33:13 +0200

    Factory-Pattern für Personen

---
+++ src/fachlogik/AbstractPersonFactory.java
@@ -0,0 +1,5 @@
+package fachlogik;
+
+public interface AbstractPersonFactory {
+	public Person createPerson(PersonType typ, String name, String plz, String wohnort, String strasse, String hausnummer);
+}
