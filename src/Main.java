ae92308c84d2893ba1fd0d7fa0e34630c586c723
Unknown <Lennart.Werner1@gmx.de>
2018-06-23 13:13:26 +0200

    Added Dependencies to make the Jar work, extracted cfg to resources package

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ import oberflaeche.Controller;
 public class Main {
 
 	public static void main(String[] args) {
-		new InitTestData();
-		new Controller();
+		try {
+			new InitTestData();
+			new Controller();
+		} catch (Exception e) {
+		}
 	}
 }
