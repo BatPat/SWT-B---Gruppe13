d982be7367a70c30cdbe68affb77889aad103a9d
Unknown <b.patti@gmx.de>
2018-07-09 09:36:42 +0200

    properties werden in der main eingelesen und stehen zur verfügung

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import oberflaeche.Controller;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Properties properties = new Properties();
 		try {
+			properties.load(new FileInputStream("resources/fahrschule.properties"));
+			String lang = properties.getProperty("lang");
+			String tax = properties.getProperty("tax");
+			//TODO verwenden der properties
+			
 			new InitTestData();
 			new Controller();
 		} catch (Exception e) {
